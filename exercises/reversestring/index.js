72cb7044690fb4e0c8cf50217f0a7ee6102a112c
Kavi Jivan <kavi@jivan.cc>
2020-11-30 08:05:30 -0500

    Finishes reverse string exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split("").reduce((rev, char) => char + rev, "");
+}
+
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// function reverse(str) {
+//   let reversed = "";
+//
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//
+//   return reversed;
+// }
+
+// function reverse(str) {
+//   let array = str.split("");
+//
+//   for (let i = 0; i < array.length / 2; i++) {
+//     [array[i], array[array.length - 1 - i]] = [
+//       array[array.length - 1 - i],
+//       array[i],
+//     ];
+//   }
+//
+//   return array.join("");
+// }
 
 module.exports = reverse;
